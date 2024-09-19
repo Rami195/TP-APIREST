b371997030ef7d3f6bfe95c3d71b12a4a45cb1a0
Rami195 <ramiromartinez3596@gmail.com>
2024-09-19 11:01:37 -0300

    Trabajo terminado

---
+++ src/main/java/org/example/persona/repositories/DomicilioRepository.java
@@ -0,0 +1,7 @@
+package org.example.persona.repositories;
+
+import org.example.persona.entities.Domicilio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DomicilioRepository extends JpaRepository<Domicilio, Long> {
+}
---
+++ src/main/java/org/example/persona/repositories/LibroRepository.java
@@ -0,0 +1,7 @@
+package org.example.persona.repositories;
+
+import org.example.persona.entities.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+}
